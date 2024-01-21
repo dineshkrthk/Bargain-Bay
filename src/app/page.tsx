501647afc25f7a911fa24065b5f44c4f586d9363
deep-69bits <lcb2021004@iiitl.ac.in>
2024-01-21 16:54:24 +0530

    Comming Soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen bg-[#FF7722] flex-col items-center justify-center">
      
-     <h1 className="text-4xl font-semibold text-white">Bargain Bay Comming Soon...</h1>
+     <h1 className="lg:text-4xl text-2xl font-semibold text-white">Bargain Bay Comming Soon...</h1>
      
     </main>
   );
