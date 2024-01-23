77fb5d2379f7d81a38e67e4a8ac1e2a44e15ca72
deep-69bits <lcb2021004@iiitl.ac.in>
2024-01-24 02:38:45 +0530

    feature section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,11 @@
 'use client'
-import Layout from '@/components/Layout/index'
-import HeroLottie from '../../public/Images/heroLottie.json'
-import { Bungee } from "next/font/google";
-import Lottie from "lottie-react";
-import Button from '@/components/Buttons';
-const inter = Bungee({ weight: ['400'], subsets: ["latin"] });
+import Hero from '@/components/Hero';
+import { Feature } from '@/components/Feature';
 export default function Home() {
   return (
     <main className="  ">
-      <Layout >
-        <div className='lg:mx-20 mx-5  py- flex justify-between items-center '>
-          <div>
-            <h1 className='my-4  mx-2 text-3xl lg:text-xl tracking-wider text-kesari'>Bargain Bay</h1>
-            <div className=' block lg:hidden'>
-              <Lottie animationData={HeroLottie} loop={true} />
-            </div>
-            <h2 className={inter.className}>
-              <span className='lg:text-8xl text-4xl  selection:bg-yellow-200'>
-                BUY <br className='hidden lg:block' /> SELL <br className='hidden lg:block' /> THRIVE
-              </span>
-            </h2>
-            <h3 className='mx-2 lg:w-[80%] w-full font-medium mt-5 selection:bg-yellow-200'>
-              Discover Savings, Unleash Security – Your Ultimate Reselling Experience at Bargain Bay.
-            </h3>
-            <Button className='my-10  w-full lg:w-[40%]' variant='primary'>
-                Explore More
-            </Button>
-          </div>
-
-          <div className='w-[700px] lg:block hidden'>
-            <Lottie animationData={HeroLottie} loop={true} />
-          </div>
-
-        </div>
-      </Layout>
+         <Hero/>
+         <Feature/>
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Bungee } from "next/font/google";
+import Lottie from "lottie-react";
+import Button from '@/components/Buttons';
+import HeroLottie from '../../public/Images/heroLottie.json'
+const inter = Bungee({ weight: ['400'], subsets: ["latin"] });
+const Hero = () => {
+    return (
+        <div className='lg:mx-20 mx-5  py- flex justify-between items-center '>
+            <div>
+                <h1 className='my-4  mx-2 text-3xl lg:text-xl tracking-wider text-kesari'>Bargain Bay</h1>
+                <div className=' block lg:hidden'>
+                    <Lottie animationData={HeroLottie} loop={true} />
+                </div>
+                <h2 className={inter.className}>
+                    <span className='lg:text-8xl text-4xl  selection:bg-yellow-200'>
+                        BUY <br className='hidden lg:block' /> SELL <br className='hidden lg:block' /> THRIVE
+                    </span>
+                </h2>
+                <h3 className='mx-2 lg:w-[80%] w-full font-medium mt-5 selection:bg-yellow-200'>
+                    Discover Savings, Unleash Security – Your Ultimate Reselling Experience at Bargain Bay.
+                </h3>
+                <Button className='my-10  w-full lg:w-[40%]' variant='primary'>
+                    Explore More
+                </Button>
+            </div>
+
+            <div className='w-[700px] lg:block hidden'>
+                <Lottie animationData={HeroLottie} loop={true} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
