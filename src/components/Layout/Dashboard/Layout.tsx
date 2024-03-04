b3f78d1a913dbb389fc4970b25ebca9e37a93320
deep-69bits <lcb2021004@iiitl.ac.in>
2024-03-04 19:58:07 +0530

    siderbar fixes

--- src/components/Layout/Dashboard/Layout.tsx
+++ src/components/Layout/Dashboard/Layout.tsx
@@ -10,11 +10,11 @@ const Layout = ({
   return (
     <div>
       <Header />
-      <div className="flex justify-between">
+      <div className="flex justify-start">
         <div className="w-[14%] ">
           <Sidebar />
         </div>
-        <div className="w-[86%]   pt-10">{children}</div>
+        <div className="  pt-10">{children}</div>
       </div>
     </div>
   );
